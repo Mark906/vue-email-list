e187c5a50286bed934f13eba06188341d78c85d0
Marco <markoka1@outlook.it>
2020-11-20 16:50:25 +0100

    Genera email via button

--- main.js
+++ main.js
@@ -3,18 +3,22 @@ var app = new Vue ({
     data: {
         title: "Genera 10 email!",
         email: [],
-        index: 0
+        index: 0,
+        status: false
     },
-    mounted(){
-        const self = this;
-
-        do{
-            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then(function(risposta){
-                if(!self.email.includes(risposta.data.response)){
-                    self.email.push(risposta.data.response);
-                }
-            });
-            self.index++;
-        } while(self.index < 10)
+    methods: {
+        generateEmail(){
+            const self = this;
+            self.status = true;
+            do{
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then(function(risposta){
+                    if(!self.email.includes(risposta.data.response)){
+                        self.email.push(risposta.data.response);
+                    }
+                });
+                self.index++;
+            } while(self.index < 10)
+        }
     }
+
 })
